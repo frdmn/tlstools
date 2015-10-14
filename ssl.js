27a63e5403c685e565395c865269ca5d12cfff35
Jonas Friedmann <j@frd.mn>
2015-10-15 00:50:04 +0200

    Add JS base file

---
+++ ssl.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+/*          _ _              _
+ *  ___ ___| | |_ ___   ___ | |___
+ * / __/ __| | __/ _ \ / _ \| / __|
+ * \__ \__ \ | || (_) | (_) | \__ \
+ * |___/___/_|\__\___/ \___/|_|___/
+ *
+ * Copyright (c) 2015 by Jonas Friedmann. This source is
+ * subject to the Microsoft Permissive License. Please see
+ * the LICENSE file for more information. All Rights Reserved.
+ */
+
+var cmdr = require('commander');
+
+// Initiate the commander.js argument setup
+cmdr
+  .version('1.1.0')
+  .command('chain [name]', 'attempt to resolve incomplete certificate chain')
+  .command('csr [query]', 'decode certificate request information')
+  .command('host', 'display TLS information for given hostname')
+  .parse(process.argv);
