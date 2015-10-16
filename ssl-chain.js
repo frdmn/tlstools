b347176a1c78ccb89346b311e995e115b6656ec4
Jonas Friedmann <j@frd.mn>
2015-10-16 19:16:05 +0200

    Rename helpers variable

--- ssl-chain.js
+++ ssl-chain.js
@@ -7,7 +7,7 @@
  */
 
 var cmdr = require('commander'),
-    helper = require('./helper');
+    helpers = require('./helper');
 
 /* Logic */
 
@@ -21,45 +21,45 @@ cmdr
 var haystack;
 
 if(cmdr.hostname){
-  helper.resolveDns(cmdr.hostname, function(ip){
-    helper.sslDecoderApi(cmdr.hostname, ip, "443", function(response){
+  helpers.resolveDns(cmdr.hostname, function(ip){
+    helpers.sslDecoderApi(cmdr.hostname, ip, "443", function(response){
       if (response.data) {
         haystack = response.data.chain['1'].key.certificate_pem;
-        helper.attemptToFixChain(haystack, function(repairResponse){
+        helpers.attemptToFixChain(haystack, function(repairResponse){
           if (repairResponse !== false) {
             console.log(repairResponse);
-            helper.success('Successfully fixed intermediate chain for "' + cmdr.hostname + '".');
-            helper.quit(0);
+            helpers.success('Successfully fixed intermediate chain for "' + cmdr.hostname + '".');
+            helpers.quit(0);
           }
         });
       } else {
-        helper.error('Couldn\'t extract certificate for "' + cmdr.hostname + '".');
+        helpers.error('Couldn\'t extract certificate for "' + cmdr.hostname + '".');
       }
     });
   });
 } else {
   // If "--input-file" is set
   if (cmdr.inputFile) {
-    haystack = helper.getFileContent(cmdr.inputFile);
-    helper.attemptToFixChain(haystack, function(repairResponse){
+    haystack = helpers.getFileContent(cmdr.inputFile);
+    helpers.attemptToFixChain(haystack, function(repairResponse){
       if (repairResponse !== false) {
         console.log(repairResponse);
-        helper.success('Successfully fixed intermediate from file "' + cmdr.inputFile + '" chain.');
-        helper.quit(0);
+        helpers.success('Successfully fixed intermediate from file "' + cmdr.inputFile + '" chain.');
+        helpers.quit(0);
       } else {
-        helper.error('Couldn\'t extract certificate from file "' + cmdr.inputFile + '".');
+        helpers.error('Couldn\'t extract certificate from file "' + cmdr.inputFile + '".');
       }
     });
   // If "--input-file" is set
   } else {
-    haystack = helper.getClipboard();
-    helper.attemptToFixChain(haystack, function(repairResponse){
+    haystack = helpers.getClipboard();
+    helpers.attemptToFixChain(haystack, function(repairResponse){
       if (repairResponse !== false) {
         console.log(repairResponse);
-        helper.success('Successfully fixed intermediate chain from clipboard.');
-        helper.quit(0);
+        helpers.success('Successfully fixed intermediate chain from clipboard.');
+        helpers.quit(0);
       } else {
-        helper.error('Couldn\'t extract certificate from clipboard.');
+        helpers.error('Couldn\'t extract certificate from clipboard.');
       }
     });
   }
