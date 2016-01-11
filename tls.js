34018f384805bcf1794da11328e04c0b8cbd2315
Jonas Friedmann <j@frd.mn>
2016-01-11 15:01:40 +0100

    Check for sub commands
    fix #12

--- tls.js
+++ tls.js
@@ -11,7 +11,8 @@
  */
 
 var cmdr = require('commander'),
-    pkg = require('./package.json');
+    pkg = require('./package.json')
+    cmds = [];
 
 // Initiate the commander.js argument setup
 cmdr
@@ -20,3 +21,13 @@ cmdr
   .command('crt', 'display TLS information for given hostname')
   .command('csr', 'decode certificate request information')
   .parse(process.argv);
+
+// Loop through available sub commands
+for (var i = 0; i < cmdr.commands.length; i++) {
+  cmds.push(cmdr.commands[i]._name);
+};
+
+// Check if sub command exist, otherwise show help
+if (cmds.indexOf(process.argv.slice(2)[0]) === -1) {
+  cmdr.help();
+};
